bfedcaab3bcffa638269da0c8ba3c4beec626909
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-29 21:54:56 -0500

    commit tree 0b5b4860fb14a47b15a16df6634230222d951dbf

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
+require 'json'
+require 'open-uri'
+require 'net/http'
+
+
 # Write your code here
+class GetRequester
+  def initialize(url)
+    @uri = URI.parse(url)
+  end
+
+  def get_response_body
+    response = Net::HTTP.get_response(@uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(self.get_response_body)
+  end
+end
